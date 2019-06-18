170168e4c7193a9229d1bf4c552297a82ebcfba6
FAxel79 <faxel79@gmail.com>
2019-06-18 23:04:37 +0300

    2 comm

--- venv/neron.py
+++ venv/neron.py
@@ -1,11 +1,12 @@
-import math
+import math, random
 class Neron:
     def __init__(self,n_svyzi = 1, fun = 1):
         self.n_svyzey = n_svyzi
         self.fun = fun
         self.vesa = []
-        self.koeff = 0.5
+        self.koeff = 0
         self.shift = 0
+        self.settype()
         self.out = 0
         for i in range (self.n_svyzey):
             self.vesa.append(1)
@@ -23,7 +24,15 @@ class Neron:
         for i in range(self.n_svyzey):
             self.vesa[i] = listves[i]
         return
+    def set_ves_R(self):
+        for i in range(self.n_svyzey):
+            self.vesa[i] = random.random()
+        return
     def settype(self,koef = 0.5, shift = 0):
         self.koeff = koef
         self.shift = shift
+        return
+    def settype_R(self):
+        self.koeff = random.random()-0.5
+        self.shift = random.random()-0.5
         return
